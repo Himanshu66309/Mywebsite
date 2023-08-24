7e44f446d14d1b8b8413e69651a9a04976f80c44
Himanshu Adhikari <142896604+HimanshuA09@users.noreply.github.com>
2023-08-24 19:09:47 +0530

    Update mynote.js

--- mynote.js
+++ mynote.js
@@ -1,8 +1,28 @@
 // JS for work experience
-
 const accordian = document.getElementsByClassName('Kyra');
     for (i = 0; i<accordian.length; i++) {
       accordian[i].addEventListener('click',function(){
         this.classList.toggle('active')
       })
     }
+
+// JS for QAE
+
+let sections = querySelectorAll('.comsec');
+let navLinks = querySelectorAll('.about-img ul a');
+
+window.onscroll = () => {
+  sections.foreach(sec => {
+    let top = Window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeifht;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+      navLinks.foreach(links => {
+        links.classList.remove('active');
+        documents.querySelectorAll('.about-img ul a [href*=' + id +']').classList.add('acive');
+      });
+    };
+  });
+};
